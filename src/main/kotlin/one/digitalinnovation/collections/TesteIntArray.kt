575b2282f09c921ce368a38c5e9a02789bf644c1
math.chiarello <chiarellomatheus@gmail.com>
2022-07-30 19:03:45 -0300

    update int array class

--- src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -1,13 +1,44 @@
+/*
+ TRABALHANDO COM ARRAY
+
+Array e List - Há diferença no Kotlin
+
+Propriedades:
+> Tamanho Fixo
+> Imutáveis
+
+IntArray() --> Informar tamanho
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package one.digitalinnovation.collections
 
 fun main() {
-    val values = IntArray(5)
+    val values = IntArray(6)
+
+    // val values2 = IntArray(1,7,6,3,2,9)
 
     values[0] = 1
     values[1] = 7
     values[2] = 6
     values[3] = 3
     values[4] = 2
+    values[5] = 9
 
     println("----------------- For Basico -----------------")
 
@@ -32,5 +63,11 @@ fun main() {
     values.sort()
     for (valor in values) {
         println(valor)
+    }
+
+    println("----------------- For Basico -----------------")
+
+    for (valor in values) {
+        println(valor + 2)
     }
 }
